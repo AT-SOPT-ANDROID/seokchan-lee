d610d220e183acaaec10e5ee489e1a057bd27e65
leeseokchan00 <112953135+leeseokchan00@users.noreply.github.com>
2025-04-13 19:43:55 +0900

    feature: Add database module

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    alias(libs.plugins.atsopt.android.library)
+    alias(libs.plugins.atsopt.android.hilt)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "org.sopt.at.database"
+}
+
+dependencies {
+    implementation(projects.core.model)
+    implementation(projects.core.data)
+}
